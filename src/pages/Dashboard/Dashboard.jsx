8344b630f3df7f5eb0dcde0f8e6e42c52f73362a
Kalhara Jayathissa <kalharajay@gmail.com>
2025-07-04 15:37:58 +0530

    By automatic main barach commit

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,3 @@
+//after setting this up, after login user will land in to this page
+//from here he can navigate between to do list and resources page and 
+//other pages by clicking the left navigation panel.
