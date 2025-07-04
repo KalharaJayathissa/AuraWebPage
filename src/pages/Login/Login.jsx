8b4c3a6a798649c52143ebdac944940c3678527d
Kalhara Jayathissa <kalharajay@gmail.com>
2025-07-04 13:14:36 +0530

    By automatic main barach commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,40 @@
-import './Login.css';
+import { AlignHorizontalCenter } from "@mui/icons-material";
+import "./Login.css";
 
 export default function Login() {
-    return(
-        <div className='container'>
-            <div className ="box">
-                <div>
-                    User name:   
-                    <input type='text'></input>
-                </div>
-                <div>
-                    Password   :   
-                    <input type="password" />
-                </div>
-                <button>Log in</button>
-            </div>
-
-            
-        </div>  
-
-
-    );
-
-
+  return (
+    <body
+      className="container"
+      style={{
+        background: "linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)",
+      }}
+    >
+      <form className="box">
+        <img
+          src="./logo.png"
+          height={175}
+          style={{ display: "block", margin: "auto" ,filter: "drop-shadow(0 0 5px rgba(102, 126, 234, 0.7))" }}
+        />
+        <div>
+          <input
+            type="text"
+            placeholder="User name"
+            required
+            id="username"
+            className="inputs"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            id="password"
+            className="inputs"
+          />
+        </div>
+        <button id="submit">Log in</button>
+      </form>
+    </body>
+  );
 }
