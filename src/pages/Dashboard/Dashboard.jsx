f08fb345e9ac7baa3974ec8323bc189814968db1
Kalhara Jayathissa <kalharajay@gmail.com>
2025-07-05 23:52:12 +0530

    By automatic main barach commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@
 //from here he can navigate between to do list and resources page and 
 //other pages by clicking the left navigation panel.
 
-import { Link } from "react-router-dom";
+
 
 export default function Dashboard(){
     return(
