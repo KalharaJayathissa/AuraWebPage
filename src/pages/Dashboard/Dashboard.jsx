112263c09aaac4e40136fa2e213992c3d050db67
Kalhara Jayathissa <kalharajay@gmail.com>
2025-07-06 22:31:57 +0530

    By automatic main barach commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,4 @@
-//after setting this up, after login user will land in to this page
-//from here he can navigate between to do list and resources page and
-//other pages by clicking the left navigation panel.
 
-//import Todolist from './Todolist/Todolist';
 import React from "react";
 import { useState } from "react";
 
@@ -16,7 +12,7 @@ export default function Dashboard() {
   return (
     <div>
       <h1>show: {label}</h1>
-      <button onClick={nextvalueupdater(() => increment(label))}> up </button>
+      <button onClick={() => nextvalueupdater(increment(label))}> up </button>
     </div>
   );
 }
