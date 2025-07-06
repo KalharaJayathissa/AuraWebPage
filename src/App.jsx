7b74cab70dc4043bbda8c19dc683f30bbb83ca99
Kalhara Jayathissa <kalharajay@gmail.com>
2025-07-06 22:08:31 +0530

    By automatic main barach commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Routes, Route, Link} from "react-router-dom";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Todolist from "./pages/Todolist/Todolist";
-import Dashboard from "./pages/Dashboard/Dashboard";
+// import Dashboard from "./pages/Dashboard/Dashboard";
 
 export default function App() {
   return (
-     <div>
-      <nav> temporary nav bar : 
-        <Link to="/">Login</Link> | {" "}
-        <Link to="/Todolist">To do list</Link>
-      </nav> 
+    <div>
+      <nav>
+        
+        temporary nav bar :<Link to="/">Login</Link> |{" "}
+        <Link to="/Todolist">To do list</Link> |
+        <Link to="/Dashboard">Dashboard</Link>
+      </nav>
 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Todolist" element={<Todolist />}></Route>
+        <Route path="/Dashboard" element={<Todolist />}></Route>
       </Routes>
-
     </div>
   );
 }
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,11 +2,12 @@
 //from here he can navigate between to do list and resources page and 
 //other pages by clicking the left navigation panel.
 
-import Todolist from './Todolist/Todolist';
+//import Todolist from './Todolist/Todolist';
+import React from "react";
 
 export default function Dashboard(){
     return(
-        <nav></nav>
+        <div> hi </div>
     );
 
 
