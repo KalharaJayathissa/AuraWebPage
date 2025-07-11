338305d8c3bf13679af5dadb8b2ac953aa6588f2
kalhara <kalharajay@gmail.com>
2025-07-12 04:19:03 +0530

    component update

--- src/pages/Todolist/Components/DataTable.jsx
+++ src/pages/Todolist/Components/DataTable.jsx
@@ -1,16 +1,29 @@
-import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Paper from "@mui/material/Paper";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+const backend_link = "http://localhost:9999/api/nonexistent-endpoint";
 
 const columns = [
   { field: "id", headerName: "Task No#", width: 70 },
   { field: "module", headerName: "Module", width: 80 },
-  { field: "task", headerName: "Task", width: 1000 ,
-    renderCell: (params) => (<div style={{whiteSpace:"normal", wordBreak:"break-word", lineHeight:1.5}}>
-      {params.value}
-    </div>)
+  {
+    field: "task",
+    headerName: "Task",
+    width: 1000,
+    renderCell: (params) => (
+      <div
+        style={{
+          whiteSpace: "normal",
+          wordBreak: "break-word",
+          lineHeight: 1.5,
+        }}
+      >
+        {params.value}
+      </div>
+    ),
   },
   {
     field: "priority",
@@ -24,17 +37,27 @@ const columns = [
     description: "Resource links",
     sortable: false,
     width: 160,
-    
   },
 ];
 
 const columnsMobile = [
   { field: "id", headerName: "Task No#", width: 35 },
   { field: "module", headerName: "Module", width: 80 },
-  { field: "task", headerName: "Task", width: 100 ,
-    renderCell: (params) => (<div style={{whiteSpace:"normal", wordBreak:"break-word", lineHeight:1.3}}>
-      {params.value}
-    </div>)
+  {
+    field: "task",
+    headerName: "Task",
+    width: 100,
+    renderCell: (params) => (
+      <div
+        style={{
+          whiteSpace: "normal",
+          wordBreak: "break-word",
+          lineHeight: 1.3,
+        }}
+      >
+        {params.value}
+      </div>
+    ),
   },
   {
     field: "priority",
@@ -48,17 +71,20 @@ const columnsMobile = [
     description: "Resource links",
     sortable: false,
     width: 160,
-    
   },
 ];
 
 //this dummy list to be replaced by fetched data late
 
-const rows = [
+const rows_dummy = [
   { id: 1, module: "OS", task: "Lec", priority: 3.5 },
-  { id: 3, module: "DB", task: "Develop the front end of the website", priority: 4.5 },
+  {
+    id: 3,
+    module: "DB",
+    task: "Develop the front end of the website",
+    priority: 4.5,
+  },
   { id: 2, module: "MA", task: "Do the tutorial", priority: 3.8 },
-
 ];
 
 const paginationModel = { page: 0, pageSize: 5 };
@@ -66,9 +92,16 @@ const paginationModel = { page: 0, pageSize: 5 };
 export default function DataTable() {
   const isMobile = useMediaQuery("(max-width:600px)");
 
-//fetching raw data from backend
+  //fetching raw data from backend
+  const [rows, setRows] = useState([]);
 
-
+  useEffect(() => {
+    axios
+      .get(backend_link)
+      .then((fetched_data) => setRows(fetched_data.data))
+      .catch((error) => console.error("Failed fetching row data"));
+    setRows(rows_dummy);
+  }, []);
 
   return (
     <Paper sx={{ height: 400, width: "100%" }}>
