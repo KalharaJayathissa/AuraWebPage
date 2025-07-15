5abd0e333d5ed1f16fcc7049e855f678c4487fad
kalhara <kalharajay@gmail.com>
2025-07-16 00:35:05 +0530

    component update

--- src/backEndURL.jsx
+++ src/backEndURL.jsx
@@ -1,8 +1,8 @@
 //This provides the backend link to any component that requirs it
 
-
-const remote_link = "https://aurabck-amghf0bpbzhfdzcj.eastasia-01.azurewebsites.net/api/v1/gettasks";
+const remote_link =
+  "https://aurabck-amghf0bpbzhfdzcj.eastasia-01.azurewebsites.net";
 
 const local_link = "http://localhost:8080";
 
-export const backendURL = remote_link
+export const backendURL = remote_link;
