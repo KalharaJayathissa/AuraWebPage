8d2eb08336ddc289b717de3d2b463447d7a4ad45
kalhara <kalharajay@gmail.com>
2025-07-17 17:46:28 +0530

    component update

--- src/pages/InClassNotes/InClassNotes.jsx
+++
@@ -1,3 +0,0 @@
-export default function InClassNotes() {
-  return <div><h3>to be implemented..</h3></div>;
-}
---
+++ src/pages/Share/Share.jsx
@@ -0,0 +1,7 @@
+export default function Share() {
+  return (
+    <div>
+      <h3>to be implemented..</h3>
+    </div>
+  );
+}
